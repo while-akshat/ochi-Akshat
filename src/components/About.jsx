d22757f79f8bdd2e52f831406ff038f8f4ec082d
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-23 03:37:44 +0530

    marquee

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 function About() {
   return (
-    <div className="w-full p-20 bg-[#cdea68] rounded-tl-3xl rounded-tr-3xl text-black leading-['3.5vw']">
-      <h1 className="font-['Neue_Montreal '] text-[3vw]">
+    <div className="w-full p-20 bg-[#cdea68] rounded-tl-3xl rounded-tr-3xl text-black leading-[4.5vw]">
+      <h1 className="font-['Neue_Montreal '] text-[4vw] tracking-tight">
         Ochi is a strategic presentation agency for forward-thinking businesses
         that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and
         hire great peo­ple.
       </h1>
+      <div className="w-full border-t-[1px] mt-20 border-[#94a74f]">
+        <div className=""></div>
+        <div className=""></div>
+      </div>
     </div>
   );
 }
