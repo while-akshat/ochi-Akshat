22b0e5fb259b2eeef6d3d2755de6748d6b5d87b6
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-23 03:57:53 +0530

    marquee

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,15 @@ function About() {
         that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and
         hire great peo­ple.
       </h1>
-      <div className="w-full border-t-[1px] mt-20 border-[#94a74f]">
+      <div className="w-full flex gap-5 border-t-[1px] pt-10 mt-20 border-[#94a74f]">
         <div className="w-1/2">
           <h1 className="text-6xl pt-10">Our Approach</h1>
-          <button className="flex gap-10 items-center mt-10 bg-zinc-900 rounded-full text-white px-8 py-0">
+          <button className=" flex gap-10 items-center mt-10 bg-zinc-900 rounded-full text-white uppercase px-5  ">
             Read More
-            <div className="w-5 h-5 bg-zinc-100 rounded-full"></div>
+            <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
           </button>
         </div>
+        <div className="w-1/2 h-[70vh] rounded-3xl bg-[#abd417] "></div>
       </div>
     </div>
   );
