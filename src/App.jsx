fc9219f96537c6d03c46522570907f04f928af7c
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-23 00:42:45 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-zinc-900">
+    <div className="w-full h-screen bg-zinc-900 text-white">
       <Navbar />
     </div>
   );
