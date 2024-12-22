b03ec39e39c7e4277ce8054dfe5c8ad4d77519e4
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-23 03:07:51 +0530

    marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Marquee() {
   return (
-    <div className="w-full py-20 rounded-3xl bg-[#004d43]">
+    <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004d43]">
       <div className="text border-t-2 border-b-2 border-zinc flex gap-10 overflow-x-hidden whitespace-nowrap ">
         <h1 className="text-[17vw] leading-none uppercase py-10  gap-10 font-bold">
           WE ARE OCHI
