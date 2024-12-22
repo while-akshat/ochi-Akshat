54af80aaad2eaab15fa2b8e0f0aee797c137dcf5
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-23 03:28:59 +0530

    marquee

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function About() {
   return (
-    <div className="w-full py-20 bg-[#cdea68] rounded-tl-3xl rounded-tr-3xl">
-      <h1>
+    <div className="w-full p-20 bg-[#cdea68] rounded-tl-3xl rounded-tr-3xl text-black leading-['3.5vw']">
+      <h1 className="font-['Neue_Montreal '] text-[3vw]">
         Ochi is a strategic presentation agency for forward-thinking businesses
         that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and
         hire great peo­ple.
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,10 +4,10 @@ function Marquee() {
   return (
     <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004d43]">
       <div className="text border-t-2 border-b-2 border-zinc flex gap-10 overflow-hidden whitespace-nowrap ">
-        <h1 className="text-[24vw] leading-none uppercase py-2  gap-10 font-[1000]">
+        <h1 className="text-[24vw] leading-none uppercase pt-2  gap-10 font-[1000]">
           WE ARE OCHI
         </h1>
-        <h1 className="text-[24vw] leading-none uppercase py-2  gap-10 font-[1000]">
+        <h1 className="text-[24vw] leading-none uppercase pt-2  gap-10 font-[1000]">
           WE ARE OCHI
         </h1>
       </div>
