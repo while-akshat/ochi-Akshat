2b59ee82eebb0ef48aec02e25fa45708a4f90988
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-23 16:57:40 +0530

    animated

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,15 +1,34 @@
+import { motion } from "motion/react";
 import React from "react";
 
 function Marquee() {
   return (
     <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004d43]">
-      <div className="text border-t-2 border-b-2 border-zinc flex gap-10 overflow-hidden whitespace-nowrap ">
-        <h1 className="text-[24vw] leading-none uppercase pt-2  gap-10 font-[1000]">
+      <div className="text border-t-2 border-b-2 border-zinc flex  overflow-hidden whitespace-nowrap ">
+        <motion.h1
+          initail={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{
+            ease: "linear",
+            duration: 10,
+            repeat: Infinity,
+          }}
+          className="text-[24vw] leading-none uppercase pt-2 pr-20 gap-10 font-[1000]"
+        >
           WE ARE OCHI
-        </h1>
-        <h1 className="text-[24vw] leading-none uppercase pt-2  gap-10 font-[1000]">
+        </motion.h1>
+        <motion.h1
+          initail={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{
+            ease: "linear",
+            duration: 10,
+            repeat: Infinity,
+          }}
+          className="text-[24vw] leading-none uppercase pt-2 pr-20  gap-10 font-[1000]"
+        >
           WE ARE OCHI
-        </h1>
+        </motion.h1>
       </div>
     </div>
   );
